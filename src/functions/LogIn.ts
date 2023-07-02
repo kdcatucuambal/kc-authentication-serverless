01fd4b71c24711dd604b6df9f240a61d8bf767da
Kevin Catucuamba <kdcatucuambal@utn.edu.ec>
2023-07-02 00:08:56 -0500

    KEC - Initial test for return token using cognito users pool

--- src/functions/LogIn.ts
+++ src/functions/LogIn.ts
@@ -1,14 +1,45 @@
 import {APIGatewayProxyHandlerV2, APIGatewayTokenAuthorizerHandler} from "aws-lambda";
 
+import {CognitoIdentityProviderClient, InitiateAuthCommand} from "@aws-sdk/client-cognito-identity-provider";
 
-export const handlerLogIn = async (event: object, context: object) => {
+
+export const handlerLogIn = async (event: any, context: any) => {
     console.log('Event: ' + JSON.stringify(event));
     console.log('Context: ' + JSON.stringify(context));
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
-            message: 'Hello World!',
-            input: event,
-        }, null, 2),
-    };
+
+    const client = new CognitoIdentityProviderClient({region: 'us-east-1'});
+    const clientId = "1nbd430stjlirh280e7gkvovfm";
+
+
+    const username: string = event["username"];
+    const password: string = event["password"];
+
+    console.log("username: " + username);
+    console.log("password: " + password);
+
+    const command = new InitiateAuthCommand({
+        AuthFlow: "USER_PASSWORD_AUTH",
+        ClientId: clientId,
+        AuthParameters: {
+            USERNAME: username,
+            PASSWORD: password
+        }
+    });
+
+    try {
+        const response = await client.send(command);
+
+        if (!response.AuthenticationResult) {
+            //Authentication failed
+            throw new Error("Authentication failed.");
+        } else {
+            return response.AuthenticationResult.AccessToken;
+        }
+
+    }catch (error){
+        console.log(error);
+        throw error;
+    } finally {
+        client.destroy();
+    }
 }
